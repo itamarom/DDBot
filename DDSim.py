ea32f1fe03c2b41b61903f71a51bf77374b7369a
Joey Geralnik <jgeralnik@gmail.com>
2016-05-28 15:27:04 +0300

    Remove trailing whitespace

--- DDSim.py
+++ DDSim.py
@@ -7,7 +7,7 @@ import pygame
 import socket
 import struct
 import select
-import win32api
+# import win32api
 
 SERVER_PORT = 1337
 
@@ -26,7 +26,7 @@ BGCOLOR = (255, 255, 255)
 
 LOOP_MS = 20
 
-BASE_SPEED = 8
+BASE_SPEED = 3
 
 MICROSWITCH_ANGLE_RANGE = 15
 
@@ -67,7 +67,7 @@ class Game(object):
                     continue
                 else:
                     self.handleEvent(event)
-                     
+
             #currentTime = time.time()
             #if currentTime - self.lastLoop < self.loopDelay:
             #    time.sleep(self.lastLoop + self.loopDelay - currentTime)
@@ -75,15 +75,15 @@ class Game(object):
             self.update()
             self.draw()
             pygame.display.flip()
-            
+
         os._exit(0)
-    
+
     def handleEvent(self, event):
         raise NotImplementedError()
 
     def draw(self):
         raise NotImplementedError()
-  
+
 class SpinningArm(object):
     def __init__(self, position, game, switchAngle=50, angle=0, speedFactor=None, reverse=False):
         self.speedFactor = speedFactor if speedFactor is not None else random.randint(92, 100) / 100.0
@@ -97,42 +97,42 @@ class SpinningArm(object):
         self.switchAngle = switchAngle
         self.switchPos = map(int, rotPoint((position[0], position[1] - 50), position, -switchAngle))
         self.last_set_servo = 0
-        
+
     def update(self):
         self.angle = (self.angle + BASE_SPEED * randfloat(0.97, 1.03) * self.currentInput * self.speedFactor) % 360
         # self.angle = (self.angle + BASE_SPEED * self.currentInput * self.speedFactor) % 360
-        
+
     def draw(self):
         rot_img = pygame.transform.rotate(self.game.arm_img, self.angle)
         rect = rot_img.get_rect()
         rect.center = self.position
         self.game.screen.blit(rot_img, rect)
         pygame.draw.circle(self.game.screen, (0, 0, 255), self.switchPos, 5, 0)
-        
+
     def setServo(self, value):
         self.currentInput = calcServoSpeed(value)
         currentTime = time.time()
         delta = currentTime - self.last_set_servo
         print "Update in", delta, "secs (", (1 / delta), " fps)"
         self.last_set_servo = currentTime
-        
+
     def getMicroswitchValue(self):
         return int(abs(self.angle - self.switchAngle) < MICROSWITCH_ANGLE_RANGE)
-        
+
 class KeyState(object):
     def __init__(self, vkey):
         self.vkey = vkey
-        
+
     def get(self):
         return win32api.GetAsyncKeyState(vkey)
-  
+
 class DDBot(Game):
     def __init__(self, no_network=False):
         super(DDBot, self).__init__()
-        
+
         self.bg_img = pygame.image.load("bgcut.png").convert()
         self.arm_img = pygame.image.load("arm2.png").convert_alpha()
-        
+
         self.right = SpinningArm(RIGHT_MOTOR_LOCATION, self, reverse=True, switchAngle=160)
         self.left = SpinningArm(LEFT_MOTOR_LOCATION, self, switchAngle=190)
         self.breadBoard = { 10: self.right.setServo, 11: self.left.setServo,
@@ -142,21 +142,21 @@ class DDBot(Game):
         self.listening_socket.bind(('', SERVER_PORT))
         self.listening_socket.listen(5)
         self.client = None
-        
+
         self.bg_rect = self.bg_img.get_rect()
         #self.bg_rect.left = -720
         self.bg_rect.top = 80
-        
+
         self.no_network = no_network
-        
+
         if not no_network:
             self.acceptClient()
-        
+
     def acceptClient(self):
         print "Accepting...",
         self.client, addr = self.listening_socket.accept()
         print "Connected!"
-        
+
     def readFromClient(self):
         rl, _, _ = select.select([self.client], [], [], 0)
         if not rl: return
@@ -177,10 +177,10 @@ class DDBot(Game):
         result = self.handleCommand(cmdType, pinNumber, value)
         if result is not None:
             self.client.sendall(struct.pack("<I", result))
-        
+
     def handleCommand(self, cmdType, pinNumber, value):
         #print "* Got command", cmdType, pinNumber, value
-        
+
         if cmdType == ServoWrite:
             print "ServoWrite(%d, %d)" % (pinNumber, value)
             self.breadBoard[pinNumber](value)
@@ -190,30 +190,30 @@ class DDBot(Game):
             print value
             return value
         elif cmdType == DigitalWrite:
-            #print "DigitalWrite(%d, %d), ignoring" % (pinNumber, value) 
+            #print "DigitalWrite(%d, %d), ignoring" % (pinNumber, value)
             pass
-        
+
     def handleEvent(self, event):
         return
         #if event.type == pygame.MOUSEMOTION:
         #    print event.pos[0], event.pos[1]
         #elif event.type == pygame.MOUSEBUTTONDOWN:
         #    print event.pos[0], event.pos[1]
-                
+
     def update(self):
         if not self.no_network:
             self.readFromClient()
         self.left.update()
         self.right.update()
         #print "Update", time.time()
-        
+
     def draw(self):
         self.screen.fill(BGCOLOR)
 
         self.screen.blit(self.bg_img, self.bg_rect)
         self.left.draw()
         self.right.draw()
-        
+
         #screen.blit(bg_img, (0, 0))
         #
         #for hit in bullet_hits:
@@ -225,7 +225,7 @@ class DDBot(Game):
         #
         #screen.blit(my_font.render("Score: " + str(score),
         #                           1, (255, 0, 0)), (50, 50))
-        
+
 def main():
     game = DDBot(no_network='-n' in sys.argv)
     game.run()
